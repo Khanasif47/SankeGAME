9c36c6722bf3afe311c238de65d77f638573ec64
Asif <aasifkkhan0911@gmail.com>
2025-12-12 00:28:33 +0530

    GameFrame

--- GameFrame.java
+++ GameFrame.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
-        this.add(new GamePanel());            // add the panel (NOT another GameFrame)
+        this.add(new GamePanel());           
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.pack();                         // uses GamePanel.getPreferredSize()
+        this.pack();                       
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
